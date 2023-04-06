6e59b15925df9ccf6b0033c56726e31a1fdb67c6
Harvy Gascon <harvy275@gmail.com>
2023-04-06 23:28:19 +0800

    90% done shit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <HelloWorld />
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { defineComponent } from 'vue';
+import HelloWorld from '../components/HelloWorld.vue';
 
-export default {
+export default defineComponent({
   name: 'HomeView',
+
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+});
 </script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Lilita+One&display=swap');
+  body{
+    font-family: 'Lilita One', cursive;
+  }
+</style>
